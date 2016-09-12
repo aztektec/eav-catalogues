ca3606bbb2dd38205b8117858e8ee88c8a8d3f7d
Omar Alvarado <omaralvaradoluna@gmail.com>
2016-09-12 00:21:17 -0500

    Entity Type Dto created

--- src/Dto/EntitiesDto.php
+++ src/Dto/EntitiesDto.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Aztektec\EAV\Dto;
 /**
  * Created by PhpStorm.
  * User: oalvarado
@@ -8,5 +9,189 @@
  */
 class EntitiesDto
 {
+    /**
+     * @var
+     */
+    protected $entity_id;
+
+    /**
+     * @var
+     */
+    protected $entity_type_id;
+
+    /**
+     * @var
+     */
+    protected $entity_parent;
+
+    /**
+     * @var
+     */
+    protected $entity_attributes_set_id;
+
+    /**
+     * @var
+     */
+    protected $entity_unique_string_key;
+
+    /**
+     * @var
+     */
+    protected $entity_created_at;
+
+    /**
+     * @var
+     */
+    protected $entity_updated_at;
+
+    /**
+     * @var
+     */
+    protected $entity_active;
+
+    /**
+     * @return mixed
+     */
+    public function getEntityId()
+    {
+        return $this->entity_id;
+    }
+
+    /**
+     * @param mixed $entity_id
+     * @return EntitiesDto
+     */
+    public function setEntityId($entity_id)
+    {
+        $this->entity_id = $entity_id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntityTypeId()
+    {
+        return $this->entity_type_id;
+    }
+
+    /**
+     * @param mixed $entity_type_id
+     * @return EntitiesDto
+     */
+    public function setEntityTypeId($entity_type_id)
+    {
+        $this->entity_type_id = $entity_type_id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntityParent()
+    {
+        return $this->entity_parent;
+    }
+
+    /**
+     * @param mixed $entity_parent
+     * @return EntitiesDto
+     */
+    public function setEntityParent($entity_parent)
+    {
+        $this->entity_parent = $entity_parent;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntityAttributesSetId()
+    {
+        return $this->entity_attributes_set_id;
+    }
+
+    /**
+     * @param mixed $entity_attributes_set_id
+     * @return EntitiesDto
+     */
+    public function setEntityAttributesSetId($entity_attributes_set_id)
+    {
+        $this->entity_attributes_set_id = $entity_attributes_set_id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntityUniqueStringKey()
+    {
+        return $this->entity_unique_string_key;
+    }
+
+    /**
+     * @param mixed $entity_unique_string_key
+     * @return EntitiesDto
+     */
+    public function setEntityUniqueStringKey($entity_unique_string_key)
+    {
+        $this->entity_unique_string_key = $entity_unique_string_key;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntityCreatedAt()
+    {
+        return $this->entity_created_at;
+    }
+
+    /**
+     * @param mixed $entity_created_at
+     * @return EntitiesDto
+     */
+    public function setEntityCreatedAt($entity_created_at)
+    {
+        $this->entity_created_at = $entity_created_at;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntityUpdatedAt()
+    {
+        return $this->entity_updated_at;
+    }
+
+    /**
+     * @param mixed $entity_updated_at
+     * @return EntitiesDto
+     */
+    public function setEntityUpdatedAt($entity_updated_at)
+    {
+        $this->entity_updated_at = $entity_updated_at;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntityActive()
+    {
+        return $this->entity_active;
+    }
+
+    /**
+     * @param mixed $entity_active
+     * @return EntitiesDto
+     */
+    public function setEntityActive($entity_active)
+    {
+        $this->entity_active = $entity_active;
+        return $this;
+    }
+
 
 }
--- src/Dto/Meta/EavEntityTypeDto.php
+++ src/Dto/Meta/EavEntityTypeDto.php
@@ -11,5 +11,160 @@ namespace Aztektec\EAV\Dto\Meta;
 
 class EavEntityTypeDto
 {
+    /**
+     * @var
+     */
+    protected $entity_type_id;
+    /**
+     * @var
+     */
+    protected $entity_type_code;
+    /**
+     * @var
+     */
+    protected $entity_type_description;
+    /**
+     * @var
+     */
+    protected $entity_type_table;
+    /**
+     * @var
+     */
+    protected $entity_type_status;
+    /**
+     * @var
+     */
+    protected $entity_type_value_table_prefix;
+    /**
+     * @var
+     */
+    protected $entity_type_ddl_description;
 
+    /**
+     * @return mixed
+     */
+    public function getEntityTypeId()
+    {
+        return $this->entity_type_id;
+    }
+
+    /**
+     * @param mixed $entity_type_id
+     * @return EavEntityTypeDto
+     */
+    public function setEntityTypeId($entity_type_id)
+    {
+        $this->entity_type_id = $entity_type_id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntityTypeCode()
+    {
+        return $this->entity_type_code;
+    }
+
+    /**
+     * @param mixed $entity_type_code
+     * @return EavEntityTypeDto
+     */
+    public function setEntityTypeCode($entity_type_code)
+    {
+        $this->entity_type_code = $entity_type_code;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntityTypeDescription()
+    {
+        return $this->entity_type_description;
+    }
+
+    /**
+     * @param mixed $entity_type_description
+     * @return EavEntityTypeDto
+     */
+    public function setEntityTypeDescription($entity_type_description)
+    {
+        $this->entity_type_description = $entity_type_description;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntityTypeTable()
+    {
+        return $this->entity_type_table;
+    }
+
+    /**
+     * @param mixed $entity_type_table
+     * @return EavEntityTypeDto
+     */
+    public function setEntityTypeTable($entity_type_table)
+    {
+        $this->entity_type_table = $entity_type_table;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntityTypeStatus()
+    {
+        return $this->entity_type_status;
+    }
+
+    /**
+     * @param mixed $entity_type_status
+     * @return EavEntityTypeDto
+     */
+    public function setEntityTypeStatus($entity_type_status)
+    {
+        $this->entity_type_status = $entity_type_status;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntityTypeValueTablePrefix()
+    {
+        return $this->entity_type_value_table_prefix;
+    }
+
+    /**
+     * @param mixed $entity_type_value_table_prefix
+     * @return EavEntityTypeDto
+     */
+    public function setEntityTypeValueTablePrefix($entity_type_value_table_prefix)
+    {
+        $this->entity_type_value_table_prefix = $entity_type_value_table_prefix;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntityTypeDdlDescription()
+    {
+        return $this->entity_type_ddl_description;
+    }
+
+    /**
+     * @param mixed $entity_type_ddl_description
+     * @return EavEntityTypeDto
+     */
+    public function setEntityTypeDdlDescription($entity_type_ddl_description)
+    {
+        $this->entity_type_ddl_description = $entity_type_ddl_description;
+        return $this;
+    }
+    
+    
 }
